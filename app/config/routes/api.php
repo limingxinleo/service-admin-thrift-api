615336901d97b023717234c4c5fad16e933b60c4
李铭昕 <715557344@qq.com>
2017-11-10 11:21:46 +0800

    修改路由文件

---
+++ app/config/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | api.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+use Phalcon\Mvc\Router\Group as RouterGroup;
+
+// Create a group with a common module and controller
+$api = new RouterGroup();
+
+// All the routes start with /api
+$api->setPrefix('/api');
+
+// 管理员登录
+$api->add('/user/login', 'App\\Controllers\\Admin\\Login::login');
+
+// 管理员基本信息
+$api->add('/user/info', 'App\\Controllers\\Admin\\User::info');
+
+$router->mount($api);
